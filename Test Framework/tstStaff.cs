babff5fed8c31766ae12366cf3238f08844a2d16
DylanPatel31 <p16199904@my365.dmu.ac.uk>
2019-01-30 12:36:03 +0000

    Creation of clsStaff + test class

---
+++ Test Framework/tstStaff.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Class_Library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_Framework
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of our class clsStaff
+            clsStaff AStaff = new clsStaff();
+            //check to see that the class in not null
+            Assert.IsNotNull(AStaff);
+        }
+    }
+}
